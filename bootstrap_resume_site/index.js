78809fe0a2bc3491406997bc3db950c0cab521c3
juploadscode <88634189+juploadscode@users.noreply.github.com>
2022-01-26 23:08:21 -0500

    Add files via upload

---
+++ bootstrap_resume_site/index.js
@@ -0,0 +1,15 @@
+let headings = ["Web Development", "Business", "Information Systems", "Software Development"];
+let counter = 0;
+let target = document.querySelector(".navbar-brand");
+let inst = setInterval(changeTime, 1500);
+let bg_video = document.getElementById("change");
+
+bg_video.muted = true;
+
+function changeTime() {
+  target.innerHTML = headings[counter];
+  counter++;
+  if (counter >= headings.length){
+    counter = 0;
+  }
+}
